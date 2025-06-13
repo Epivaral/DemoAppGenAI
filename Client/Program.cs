e865d4d2cc77132a3db923cd2bb2f9362eca9593
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-06-13 19:43:47 +0000

    fix assembly errors

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
