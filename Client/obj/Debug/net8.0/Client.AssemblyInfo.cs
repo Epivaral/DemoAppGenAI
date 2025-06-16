a4cb1474ed4d861e6d08c0bd18d6960c69bc5c28
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2025-06-16 15:20:50 +0000

    UI load message

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6e6b9ebf5d44bd0c57a3520204f5462ba6db9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca94f799c72ec1b533edb6c6abfce5443cd08abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
